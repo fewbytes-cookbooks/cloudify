e13903b7c4aee726450d7f03902a72b0a39e4df4
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-06-03 16:22:57 +0300

    Handler should be installed with cookbook_file

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,7 +1,11 @@
 include_recipe "ohai"
 
+cookbook_file ::File.join(node["chef_handler"]["handler_path"], "handlers", "cloudify.rb") do
+  source "handlers/cloudify.rb"
+end
+
 chef_handler "Cloudify::ChefHandlers::AttributesDumpHandler" do
-  source "handlers/cloudify"
+  source ::File.join("handlers", "cloudify.rb")
   action :enable
 end
 
