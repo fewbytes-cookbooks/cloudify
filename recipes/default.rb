23fd2d878bc6c8c820b865e6354e0269a040d0da
Avishai Ish-Shalom <avishai@fewbytes.com>
2013-06-03 17:17:48 +0300

    Fixed handler paths

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,12 @@
 include_recipe "ohai"
 include_recipe "chef_handler"
 
-cookbook_file ::File.join(node["chef_handler"]["handler_path"], "handlers", "cloudify.rb") do
+cookbook_file ::File.join(node["chef_handler"]["handler_path"], "cloudify.rb") do
   source "handlers/cloudify.rb"
 end
 
 chef_handler "Cloudify::ChefHandlers::AttributesDumpHandler" do
-  source ::File.join("handlers", "cloudify.rb")
+  source ::File.join(node['chef_handler']['handler_path'], "cloudify.rb")
   action :enable
 end
 
